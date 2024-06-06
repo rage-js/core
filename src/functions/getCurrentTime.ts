44e7f9f7d395099006c1000eb41f2d2ce2d389cb
Maghish <mail4mithran@gmail.com>
2024-06-06 21:52:47 +0530

    gh-36: Working on PushAfterInterval pt-7

--- src/functions/getCurrentTime.ts
+++ src/functions/getCurrentTime.ts
@@ -2,23 +2,27 @@
  * Get current time to log on console
  */
 function getCurrentTime() {
-  const now = new Date();
+  try {
+    const now = new Date();
 
-  // Get hours (0-23)
-  let hours: any = now.getHours();
+    // Get hours (0-23)
+    let hours: any = now.getHours();
 
-  // Get minutes (0-59)
-  let minutes: any = now.getMinutes();
+    // Get minutes (0-59)
+    let minutes: any = now.getMinutes();
 
-  // Get seconds (0-59)
-  let seconds: any = now.getSeconds();
+    // Get seconds (0-59)
+    let seconds: any = now.getSeconds();
 
-  // Format the time string with leading zeros (optional)
-  hours = String(hours).padStart(2, "0");
-  minutes = String(minutes).padStart(2, "0");
-  seconds = String(seconds).padStart(2, "0");
+    // Format the time string with leading zeros (optional)
+    hours = String(hours).padStart(2, "0");
+    minutes = String(minutes).padStart(2, "0");
+    seconds = String(seconds).padStart(2, "0");
 
-  return `${hours}:${minutes}:${seconds}`;
+    return `${hours}:${minutes}:${seconds}`;
+  } catch (error: any) {
+    console.error(error); // Throw error, do not just console log
+  }
 }
 
 export default getCurrentTime;
