b1d6d9496757edd500a7290a8125235b56835f64
Maghish <mail4mithran@gmail.com>
2024-06-06 20:42:49 +0530

    gh-36: Working on PushAfterInterval pt-4

--- src/functions/formatLog.ts
+++ src/functions/formatLog.ts
@@ -11,24 +11,30 @@ function formatLog(
 ) {
   try {
     let emoji = "⚙️";
+    let color = chalk.white;
 
     if (type === "config") {
       emoji = "⚙️";
+      color = chalk.white;
     }
     if (type === "pull") {
       emoji = "↙️";
+      color = chalk.white;
     }
     if (type === "push") {
       emoji = "↗️";
+      color = chalk.white;
     }
     if (type === "warning") {
       emoji = "⚠️";
+      color = chalk.yellow;
     }
     if (type === "error") {
       emoji = "🛑";
+      color = chalk.red;
     }
 
-    return `${emoji} ${chalk.bold(`[${getCurrentTime()}]`)} ${message}`;
+    return `${emoji} ${chalk.bold(`[${getCurrentTime()}]`)} ${color(message)}`;
   } catch (error: any) {
     console.log(
       `🛑 ${chalk.bold(`[${getCurrentTime()}]`)} Unexpected error occurred!`
