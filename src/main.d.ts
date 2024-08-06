73321aa1cb081c49e30b8eebb8f271073ea085b9
Maghish <mail4mithran@gmail.com>
2024-08-06 10:50:08 +0530

    Added JSDoc comments for every attributes (excluding sub-attributes) in main.d.ts

--- src/main.d.ts
+++ src/main.d.ts
@@ -1,19 +1,51 @@
 interface configurationType {
+  /**
+   * Choose which method does the application use
+   * PAI - Push After Interval
+   * NI - No Interval
+   */
   method: "PAI" | "NI";
+
+  /**
+   * Method specifc settings, each method will likely have specific mandatory settings
+   */
   methodSpecificSettings: {
     // Push After Interval
     interval?: number;
   };
+
+  /**
+   * Choose which database does the application use
+   */
   databaseType: "MongoDB";
+
+  /**
+   * Specific settings for every database type, each database will likely have specific mandatory settings
+   */
   databaseSpecificSettings: {
     // MongoDB
     secretKey?: string;
     dbs?: string[];
     excludeCollections?: string[];
   };
+
+  /**
+   * The directory where the local database is located for the application
+   */
   outDir: string;
+
+  /**
+   * An optional attribute to whether fetch the database on the start of the application or not
+   * Only PushAfterInterval and PushOnUpdate uses this attribute. NoInterval doesn't, as it fetches the database on start no matter what.
+   */
   fetchOnFirst?: boolean;
-  loopStartDelay?: number;
+
+  /**
+   *  Always before a method instance starts, it sleeps for some time which lets the application to connect MongoDB and then starts.
+   *  That delay is an estimate of how long it would take for the application to connect to MongoDB.
+   *  This process can't be done synchronously, hence had to come up with this method.
+   */
+  loopStartDelay: number;
 }
 
 export { configurationType };
